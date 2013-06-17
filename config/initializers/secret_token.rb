3eb51e0e17b600244ae5612be01f7ad609a17a99
Janusz Liro <janlir@wp.pl>
2013-06-17 11:11:38 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtistSpace::Application.config.secret_token = 'f48730393acccaf3e0f7c6bb3d9c1ea4d67a934c965ca33c7eda3115f64b3c504ec6b41c42c4f4dfa6bec6883f00d2cdc48a165a96af550b147f669d335247d3'
